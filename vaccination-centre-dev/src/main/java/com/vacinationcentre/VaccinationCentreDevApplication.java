8dfaba3d1294c286dec16a70f2e33290a87b3cc5
SAM15 <SAM15@DESKTOP-8NJC4FI>
2023-01-20 23:58:17 +0530

    first commit

---
+++ vaccination-centre-dev/src/main/java/com/vacinationcentre/VaccinationCentreDevApplication.java
@@ -0,0 +1,13 @@
+package com.vacinationcentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationCentreDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationCentreDevApplication.class, args);
+	}
+
+}
---
+++ vaccination-centre-dev/src/test/java/com/vacinationcentre/VaccinationCentreDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacinationcentre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationCentreDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
