a95264e3c4267320c43e4d126d8a4e84eb243072
Administrator <Administrator@192.168.1.37>
2023-01-21 14:02:48 +0530

    new service added

---
+++ citizen-center/src/test/java/com/citizen/center/CitizenCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citizen.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
